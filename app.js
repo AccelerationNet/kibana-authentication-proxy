10bd41abb09d9a24e19908c5768ef3fd43ee4b76
Nathan Bird <nathan@acceleration.net>
2014-02-28 14:55:15 -0500

    Pull kibana proxying config out into separate module
    Trying to make app.js really short and easy to read.

--- app.js
+++ app.js
@@ -48,12 +48,9 @@ require('./lib/cas-auth.js').configureCas(express, app, config);
 require('./lib/es-proxy').configureESProxy(app, config.es_host, config.es_port,
           config.es_username, config.es_password);
 
-// Serve config.js for kibana3
-// We should use special config.js for the frontend and point the ES to __es/
-app.get('/config.js', kibana3configjs);
 
-// Serve all kibana3 frontend files
-app.use('/', express.static(__dirname + '/kibana/src'));
+require('./lib/kibana-proxy').configure(app, config);
+
 
 
 run();
@@ -71,33 +68,4 @@ function run() {
   console.log('Server listening on ' + config.listen_port);
 }
 
-function kibana3configjs(req, res) {
- 
 
-  function getKibanaIndex() {
-    var raw_index = config.kibana_es_index;
-    var user_type = config.which_auth_type_for_kibana_index;
-    var user;
-    if (raw_index.indexOf('%user%') > -1) {
-      if (user_type === 'google') {
-        user = req.googleOauth.id;
-      } else if (user_type === 'basic') {
-        user = req.user;
-      } else if (user_type === 'cas') {
-        user = req.session.cas_user_name;
-      } else {
-        user = 'unknown';
-      }
-      return raw_index.replace(/%user%/gi, user);
-    } else {
-      return raw_index;
-    }
-  }
-
-  res.setHeader('Content-Type', 'application/javascript');
-  res.end("define(['settings'], " +
-    "function (Settings) {'use strict'; return new Settings({elasticsearch: '/__es', default_route     : '/dashboard/file/default.json'," +
-      "kibana_index: '" +
-      getKibanaIndex() +
-      "', panel_names: ['histogram', 'map', 'pie', 'table', 'filtering', 'timepicker', 'text', 'hits', 'column', 'trends', 'bettermap', 'query', 'terms', 'sparklines'] }); });");
-}
